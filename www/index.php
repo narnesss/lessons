08bed1adc0b20cc6aca1537f75bd2aff7a200586
narnes <narnesss@mail.ru>
2022-12-14 19:10:13 +0300

    Patterns

--- www/index.php
+++ www/index.php
@@ -1,9 +1,27 @@
 <?php
 
-use Root\CommsManager;
+use Root\Archer;
+use Root\Army;
+use Root\LaserCannonUnit;
 
 
 if (file_exists($_SERVER["DOCUMENT_ROOT"] . "/vendor/autoload.php")) {
     require_once($_SERVER["DOCUMENT_ROOT"] . "/vendor/autoload.php");
 }
 
+
+$main_army = new Army();
+
+$main_army->addUnit(new Archer());
+$main_army->addUnit(new LaserCannonUnit());
+
+$sub_army = new Army();
+
+$sub_army->addUnit(new Archer());
+$sub_army->addUnit(new Archer());
+$sub_army->addUnit(new Archer());
+
+$main_army->addUnit($sub_army);
+
+// все вычисления выполняются за кулисами
+print "Атака с силой: {$main_army->bombardStrenght()}\n";
